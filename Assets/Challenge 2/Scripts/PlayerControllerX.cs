3a76cf0c81738eca4c1e18b5260093ff8c4653ed
keres.pls <keres.pls@tutanota.com>
2024-03-02 13:39:44 -0800

    AB#27 | Completed Challenge 2

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -6,13 +6,37 @@ public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
 
+    private float spawnDogInterval = 1.0f;
+    private float spawnDogCounter = 0.0f;
+    private bool coolDownDog = false;
+
     // Update is called once per frame
     void Update()
     {
-        // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+ 
+        if (Input.GetKeyDown(KeyCode.Space) && !coolDownDog)
         {
-            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            SpawnDog();
+            coolDownDog = true;
+        }
+        if (coolDownDog)
+        {
+            if (spawnDogCounter < spawnDogInterval)
+            {
+                spawnDogCounter += Time.deltaTime;
+            }
+            else
+            {
+                spawnDogCounter = 0.0f;
+                coolDownDog = false;
+            }
         }
     }
+
+
+    void SpawnDog()
+    {
+        Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+    }
+
 }
--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -9,24 +9,46 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnLimitXLeft = -22;
     private float spawnLimitXRight = 7;
     private float spawnPosY = 30;
-
-    private float startDelay = 1.0f;
     private float spawnInterval = 4.0f;
 
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// Extents on x axis that animals can be spawned within
+    /// </summary>
+    public float xRange = 15;
+
+    private float delayCounter = 0.0f;
+    /// <summary>
+    /// The maximum variability between spawn delays
+    /// </summary>
+    public float spawnRateMaxFuzz = 1.0f;
+
+    private float spawnRateFuzz;
+
+
+
+    void Update()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        if (delayCounter < spawnInterval + spawnRateFuzz)
+        {
+            delayCounter += Time.deltaTime;
+        }
+        else
+        {
+            delayCounter = 0.0f;
+            spawnRateFuzz = Random.Range(spawnRateMaxFuzz * -1, spawnRateMaxFuzz);
+            SpawnRandomBall();
+        }
     }
 
     // Spawn random ball at random x position at top of play area
-    void SpawnRandomBall ()
+    void SpawnRandomBall()
     {
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
-
+        int ballIndex = Random.Range(0, ballPrefabs.Length);
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
     }
+
 
 }
